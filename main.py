9c577ffe314fbb9f5622c8f1ce9bdfaf2be13a22
renganomics <orengasi@gmail.com>
2024-08-31 17:56:12 +0100

    Initial Commit: Implement ChatbotWindow class

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+
+from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow, QLineEdit, \
+                            QPushButton, QTextEdit
+
+
+class ChatbotWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setMinimumSize(700, 500)
+
+        # Add chat area widget
+        self.chat_area = QTextEdit(self)
+        self.chat_area.setGeometry(10, 10, 480, 320)
+        self.chat_area.setReadOnly(True)
+
+        # Add input field widget
+        self.input_field = QLineEdit(self)
+        self.input_field.setGeometry(10, 340, 480, 40)
+
+        # Add the button
+        self.button = QPushButton("Send", self)
+        self.button.setGeometry(500, 340, 100, 40)
+
+        self.show()
+
+
+class Chatbot:
+    pass
+
+
+app = QApplication(sys.argv)
+main_window = ChatbotWindow()
+sys.exit(app.exec())
